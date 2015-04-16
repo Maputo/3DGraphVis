cd7babb00f5bdcdfa08c867b21e18d88fed412da
Haris Ljajic <haris.ljajic@gmail.com>
2015-04-16 17:19:14 +0200

    start working on 3D graph visualization

---
+++ graph.js
@@ -0,0 +1,18 @@
+// Create a 3d graph  within d3 selection parent.
+function scatterPlot3d( parent )
+{
+  var x3d = parent  
+    .append("x3d")
+      .style( "width", parseInt(parent.style("width"))+"px" )
+      .style( "height", parseInt(parent.style("height"))+"px" )
+      .style( "border", "none" )
+      
+  var scene = x3d.append("scene")
+
+  scene.append("orthoviewpoint")
+     .attr( "centerOfRotation", [5, 5, 5])
+     .attr( "fieldOfView", [-5, -5, 15, 15])
+     .attr( "orientation", [-0.5, 1, 0.2, 1.12*Math.PI/4])
+     .attr( "position", [8, 4, 15])
+
+}
