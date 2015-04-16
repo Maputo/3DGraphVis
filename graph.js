b046c7e6dbe3de33cc608b28d41ae8bb915600ff
Haris Ljajic <haris.ljajic@gmail.com>
2015-04-16 21:20:12 +0200

    add function for drawing nodes

--- graph.js
+++ graph.js
@@ -1,18 +1,34 @@
 // Create a 3d graph  within d3 selection parent.
-function scatterPlot3d( parent )
-{
+function scatterPlot3d( parent ) {
   var x3d = parent  
     .append("x3d")
-      .style( "width", parseInt(parent.style("width"))+"px" )
-      .style( "height", parseInt(parent.style("height"))+"px" )
-      .style( "border", "none" )
-      
+    .style( "width", "99%" )
+    .style( "height", "99%" )
+    .style( "border", "none" )
+  
+  
   var scene = x3d.append("scene")
 
   scene.append("orthoviewpoint")
-     .attr( "centerOfRotation", [5, 5, 5])
-     .attr( "fieldOfView", [-5, -5, 15, 15])
-     .attr( "orientation", [-0.5, 1, 0.2, 1.12*Math.PI/4])
-     .attr( "position", [8, 4, 15])
+    .attr( "centerOfRotation", [1, 1, 1])
+    .attr( "fieldOfView", [-3, -3, 15, 15])
+    .attr( "orientation", [-0.5, 1, 0.2, 1.12*Math.PI/4])
+    .attr( "position", [8, 4, 15])
 
+
+  function drawNode() {
+    var nodes = scene
+      .append("shape");
+    
+    nodes
+      .append("appearance")
+      .append("material");
+    
+    nodes
+      .append("sphere")
+      .attr("radius", "0.2");
+  }
+
+
+  drawNode();
 }
