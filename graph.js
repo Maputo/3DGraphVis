2a947beb667213b0ec8992471de5d05a0b801827
Haris Ljajic <haris.ljajic@gmail.com>
2015-04-16 23:13:54 +0200

    clean up

--- graph.js
+++ graph.js
@@ -1,6 +1,29 @@
 // Create a 3d graph  within d3 selection parent.
 function graph3d( parent ) {
   
+  // Assign handlers immediately after making the request,
+  // and remember the jqxhr object for this request
+  var jqxhr = $.getJSON( "http://berndmalle.com/akhci/graphs/graph_small.json", function() {
+    console.log( "success" );
+  })
+    .done(function() {
+      console.log( "second success" );
+    })
+    .fail(function() {
+      console.log( "error" );
+    })
+    .always(function() {
+      console.log( "complete" );
+    });
+  
+  // Perform other work here ...
+  
+  // Set another completion function for the request above
+  jqxhr.complete(function() {
+    console.log( "second complete" );
+  });
+
+
   function Node(x, y, z) {
     this.x = x;
     this.y = y;
